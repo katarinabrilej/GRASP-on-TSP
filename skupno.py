745cd1cc1f336fd26cfe440a974036bcea6e51f3
Katarina Brilej <katarina.brilej@gmail.com>
2019-01-04 16:39:49 +0100

    ustvarjena datoteka skupno v kateri so shranjene glavne funkcije celotnega projekta

---
+++ skupno.py
@@ -0,0 +1,20 @@
+import GRASP as GRASP
+import ilp as ILP
+import uvoz as uv
+
+# funkcije
+
+##GRASP.slovar_razdalj(g)
+##GRASP.dolzina_poti(slovar,n,pot)
+##GRASP.local_search(g,alpha,iter, metoda)
+
+# local_search je funkcija, ki zažene algoritem in vrne najkrajšo pot
+
+# metoda je lahko dva_opt ali tri_opt
+# g je incidenčna matrika
+# iter je število iteracij
+# alpha je število začetni približkov (moč RCL)
+# slovar je slovar razdalj
+# n je velikost matrike/grafa/cikla
+
+
